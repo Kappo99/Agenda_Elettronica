e4044f6e54cd9308efe4c9f7bf52ca69aa64ca7d
Andrea Cappone <andrea30.4.99@gmail.com>
2025-05-10 16:13:50 +0200

    feat: update header branding and layout in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import KMsolution from '../images/KMsolution_black.svg';
+import DailyCare from '../images/DailyCare.png';
 import { MdLogout, MdPerson } from 'react-icons/md';
 import { Link, useLocation } from 'react-router-dom';
 import { logout } from '../redux/slices/authSlice';
@@ -26,15 +26,15 @@ function Header() {
         <>
             <nav className='w-full bg-white shadow-lg text-sm mb-6'>
 
-                <div className='container w-full py-3 lg:py-5'>
+                <div className='container w-full py-4'>
 
-                    <div className='flex flex-col md:flex-row items-center justify-between gap-x-10 gap-y-6'>
+                    <div className='flex flex-row items-center justify-between gap-x-10 gap-y-6'>
 
                         <Link to='/'>
-                            <img src={KMsolution} className='w-auto h-8' alt='DailyCare' />
+                            <img src={DailyCare} className='w-auto h-16 md:h-12' alt='DailyCare' />
                         </Link>
 
-                        <span className='flex-1 h2 text-center'>DailyCare</span>
+                        <span className='flex-1 h2 text-center hidden md:block'>DailyCare</span>
 
                         <div className='flex justify-end'>
                             {location.pathname === '/profilo' ? (
